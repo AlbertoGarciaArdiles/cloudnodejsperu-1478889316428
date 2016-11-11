fbb71584572e2ab9902cf8f807e10b434ed7b2c1
AlbertoGarciaArdiles <afgarcia@pe.ibm.com>
2016-11-11 13:51:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+//AG Starts New Relic
+require("./newrelic").initialize();
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
